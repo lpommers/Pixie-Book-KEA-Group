e7516f9fc64551b08a0bdb127e00bc56dcd78d0f
Luke Pommersheim <lpommers@gmail.com>
2013-10-25 13:05:11 +0200

    Audio and scenes
    Added the two audio files, they're set to auto play.
    
    Added the tower images and such

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 $(function(){
 	$('.scene').hide();
 
-	$('div a').bind('click', function(event){
+	$('#scene1-button').bind('click', function(event){
 		var $anchor = $(this);
-
-		var scene = $($(this).attr('href'));
-
-		scene.show();
-
-
+		$('#scene2').show();
 		$('html, body').stop().animate({
 			scrollTop: $($anchor.attr('href')).offset().top}, 2000);
+
+			event.preventDefault();
+		});
+
+	$('#scene2-button').bind('click', function(event){
+		var $anchor = $(this);
+		$('#scene3, #scene2-5').show();
+		
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top}, 2000);
+
+			event.preventDefault();
+		});
+
+	$('#scene2-button').bind('click', function(event){
+		var $anchor = $(this);
+		$('#scene3').show();
+		
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top}, 5000);
 
 			event.preventDefault();
 		});
