63df5ae253dbbba6c6c0e60e44d148e9ab7f7880
chikaboo1 <chikaboo@Natashas-MacBook-Pro-2.local>
2013-10-27 17:54:45 +0100

    Created full width

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(function(){
 	// this hides all the scenes except the title scene when the page loads
-	$('.scene').hide();
+	$('.scene, .scene-wrapper').hide();
 
 
 	// This is the first click. It leaves the title screen and scrolls the first screen. The first audio scene is played on click.
 	$('#title-button').bind('click', function(event){
 		var $anchor = $(this);
+		$('#scene1-wrapper').show();
 		$('#scene1').show();
 
 		//this is the scrolling animation
@@ -22,7 +23,9 @@ $(function(){
     // This is the second click. It leaves the title screen and scrolls to the top of the tower. 
 	$('#scene1-button').bind('click', function(event){
 		var $anchor = $(this);
+		$('#scene2-wrapper').show();
 		$('#scene2').show();
+		
 
 		//the scrolling
 		$('html, body').stop().animate({
@@ -34,6 +37,7 @@ $(function(){
 	// This is the second click. It leaves the title screen and scrolls to the bottom of the tower. 
 	$('#scene2-button').bind('click', function(event){
 		var $anchor = $(this);
+		$('#scene3-wrapper, #scene2-5-wrapper').show();
 		$('#scene3, #scene2-5').show();
 		
 		//the scrolling
@@ -47,6 +51,7 @@ $(function(){
 	//this will go to whatever next. 
 	$('#scene2-button').bind('click', function(event){
 		var $anchor = $(this);
+		$('#scene3-wrapper').show();
 		$('#scene3').show();
 		
 		//the scrolling
